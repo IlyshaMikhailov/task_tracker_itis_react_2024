a61579703cf0359c6d7a8b8e089275a548bd6df5
IlyshaMikhailov <mix.il2812@gmail.com>
2024-04-16 18:51:49 +0300

    Lesson23

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useState, useMemo } from "react";
+import { ApolloProvider } from "@apollo/client";
+
+import NotifierContext from "./context/NotifierContext";
+import apolloClient from "./lib/apolloClient";
+
 import HomePage from "./pages/HomePage";
+import InfoPage from "./pages/InfoPage";
+import ProjectsPage from "./pages/ProjectsPage";
 
 const App = () => {
+  const [message, setMessage] = useState("");
+  const [messageType, setMessageType] = useState("");
+
+  const context = useMemo(
+    () => ({
+      message,
+      setMessage: (text) => setMessage(text),
+      clearMessage: () => setMessage(""),
+      messageType,
+      setMessageType: (text) => setMessageType(text),
+      clearMessageType: () => setMessageType(""),
+    }),
+    [message, messageType]
+  );
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <NotifierContext.Provider value={context}>
+      <ApolloProvider client={apolloClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/info" element={<InfoPage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
+      </ApolloProvider>
+    </NotifierContext.Provider>
   );
 };
 
