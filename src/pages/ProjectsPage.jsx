6626f0144d736648f535116ddd0fb7c21daac778
IlyshaMikhailov <mix.il2812@gmail.com>
2024-04-16 18:52:58 +0300

    Lesson23

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import DefaultTemplate from "../components/template/DefaultTemplate";
-
-
-const ProjectPage = () => {
-    return (
-        <div>
-            <DefaultTemplate />
-            <h1>It is Project Page</h1>
-        </div>
-    );
-};
-
-export default ProjectPage;
+// import { useProjects } from "src/lib/hooks/project";
+// import DefaultTemplate from "../components/templates/DefaultTemplate";
+// // import ProjectsTable from '../components/organisms/ProjectsTable';
+//
+// const ProjectsPage = () => {
+//   const { projects, loading, error } = useProjects();
+//   console.log(projects, loading, error)
+//
+//   return (
+//     <DefaultTemplate>
+//       <h2>Projects List</h2>
+//
+//       <ProjectsTable />
+//     </DefaultTemplate>
+//   );
+// };
+//
+// export default ProjectsPage;
